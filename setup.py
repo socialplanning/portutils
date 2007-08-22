b394cb6d7b03a76d6df700cab5847fbf7ea19542
Jeff Hammel <jhammel@mozilla.com>
2007-08-22 17:50:19 +0000

    adding portutils from topp.utils -- needs integration work
    git-svn-id: https://svn.socialplanning.org/svn/standalone/portutils/trunk@8373 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0'
+
+setup(name='portutils',
+      version=version,
+      description="",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      keywords='topp openplans',
+      author='The Open Planning Project',
+      author_email='jhammel@openplans.org',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['topp'],
+      include_package_data=True,
+      zip_safe=False,
+      entry_points="""
+      # -*- Entry points: -*-
+      
+      """,
+      )
