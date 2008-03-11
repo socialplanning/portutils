81297f0bc1913f1f13e9432ca56dd16afaf0f3e8
Paul Winkler <slinkp@gmail.com>
2008-03-11 19:04:44 +0000

    In unix, exiting non-zero == error. This was backwards.
    git-svn-id: https://svn.socialplanning.org/svn/standalone/portutils/trunk@14350 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- portutils/__init__.py
+++ portutils/__init__.py
@@ -8,8 +8,9 @@ def portcheck_main(args=sys.argv[1:]):
     ports = portcheck(*args)
     for i in ports:
         print '%s: %s' % (i, ports[i])
+    return 0
         
 def portkill_main(args=sys.argv[1:]):
     """commandline front-end to portkill"""
-    return portkill(*args)
+    return int(not portkill(*args))
     
